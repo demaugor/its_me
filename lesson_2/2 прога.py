5c5562174f485f51a9472649cfaa21d00d3146c4
demaugor <118732056+demaugor@users.noreply.github.com>
2023-05-29 00:47:53 +0400

    Update 2 прога.py

--- lesson_2/2 прога.py
+++ lesson_2/2 прога.py
@@ -1,4 +1,9 @@
-number = int(input("Введите число: ")) 
-if number % 3 == 0: print("Fizz" , end="") 
-if number % 5 == 0: print("Buzz")
-else: print("Число не делится на 3 и на 5 без остатка")
+number = int(input("Введите число: "))
+
+if number % 3 == 0:
+    print("Fizz" , end="") 
+if number % 5 == 0:
+    print("Buzz")
+
+else:
+    print("Число не делится на 3 и на 5 без остатка")
