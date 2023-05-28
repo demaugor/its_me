18a13c0f5ba8bce5c5272d91410f1d6dd38ec528
demaugor <118732056+demaugor@users.noreply.github.com>
2023-05-28 20:17:58 +0400

    Update 1пипец.py

--- lesson_2/1пипец.py
+++ lesson_2/1пипец.py
@@ -1,6 +1,6 @@
-day = "28"
-month = "5"
-year = "2023"
+day = 28
+month = 5
+year = 2023
 day_of_birth = int(input("Введите день своего рождения: "))
 month_of_birth = int(input("Введите месяц своего рождения: "))
 year_of_birth = int(input("Введите год своего рождения: "))
@@ -9,7 +9,7 @@ if day_of_birth < 1 or day_of_birth > 31:
 elif month_of_birth < 1 or month_of_birth > 12:
     print("Ошибка: неверный месяц рождения")
 elif year_of_birth > 2023: print("Ошибка: неверный год рождения")
-if 2023 - year_of_birth > 18: print("путь открыт")
-elif 2023 - year_of_birth == 18 and month_of_birth < 12: print("путь открыт")
-elif 2023 - year_of_birth == 18 and month_of_birth == 12 and day_of_birth <= 31: print("путь открыт")
+if year - year_of_birth > 18: print("путь открыт")
+elif year - year_of_birth == 18 and month_of_birth < 12: print("путь открыт")
+elif year - year_of_birth == 18 and month_of_birth == 12 and day_of_birth <= 31: print("путь открыт")
 else: print("куда лезешь малой")
