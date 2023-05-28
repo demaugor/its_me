0aaf669e3a334ba58264b16826a1b4fc1d55c834
demaugor <118732056+demaugor@users.noreply.github.com>
2023-05-29 00:46:08 +0400

    Update 3 задачка.py

--- lesson_2/3 задачка.py
+++ lesson_2/3 задачка.py
@@ -1,8 +1,14 @@
 price = float(input("Введите стоимость товара: "))
-if price > 500: discount = 20
-elif price > 100: discount = 15
-elif price > 80: discount = 10
+
+if price > 500:
+    discount = 20
+elif price > 100:
+    discount = 15
+elif price > 80:
+    discount = 10
 else: discount = 5
+
 discount_amount = price * discount / 100
 price_with_discount = price - discount_amount
+
 print(f"Цена со скидкой {discount}%: {price_with_discount}")
