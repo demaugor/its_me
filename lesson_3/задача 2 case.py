13c6fc95240053cbb7ae5bbccf97e2a88458942a
demaugor <118732056+demaugor@users.noreply.github.com>
2023-06-06 01:36:22 +0400

    терь норм

--- lesson_3/задача 2 case.py
+++ lesson_3/задача 2 case.py
@@ -24,13 +24,13 @@ match HttpCode:
     case 510:
         print("Not Extended")
         
-if HttpCode > 103:
+if HttpCode > 103 and HttpCode < 200:
     print("Неизвестный код для группы 1xx")
-elif HttpCode > 207:
+elif HttpCode > 207 and HttpCode < 300:
     print("Неизвестный код для группы 2xx")
-elif HttpCode > 308:
+elif HttpCode > 308 and HttpCode < 400:
     print("Неизвестный код для группы 3xx")
-elif HttpCode > 499:
+elif HttpCode > 409 and HttpCode < 500:
     print("Неизвестный код для группы 4xx")
-elif HttpCode > 599:
+elif HttpCode > 511:
     print("Неизвестный код для группы 5xx")
