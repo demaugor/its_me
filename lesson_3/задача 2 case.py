f6bc73b97650f082ffabf2d626e4c1363054b5c2
demaugor <118732056+demaugor@users.noreply.github.com>
2023-06-07 20:41:20 +0400

    всё ура надеюсь

--- lesson_3/задача 2 case.py
+++ lesson_3/задача 2 case.py
@@ -1,5 +1,6 @@
-HttpCode = int(input("Введите http-код: "))
-match HttpCode:
+Http_Code = int(input("Введите http-код: "))
+
+match Http_Code:
     
     case 100:
         print("Continue")
@@ -7,30 +8,44 @@ match HttpCode:
         print("Processing")
     case 103:
         print("Early Hints")
+    case code1xx if Http_Code > 103 and Http_Code < 200:
+        print("Неизвестный код для группы 1xx")
+            
     case 200:
-        print("Continue")
+        print("OK")
+    case 203:
+        print ("Non-Authoritative")
     case 204:
-        print("Processing")
+        print("No content")
+    case 207:
+        print("Multi-Status")
+    case code2xx  if Http_Code > 207 and Http_Code < 300:
+        print("Неизвестный код для группы 2xx")
+            
+    case 300:
+        print("Move Choices")
     case 302:
         print("Found")
     case 305:
         print("Use Proxy")
+    case code3xx if Http_Code > 308 and Http_Code < 400:
+        print("Неизвестный код для группы 3xx")
+            
     case 404:
         print("Not Found")
     case 409:
         print("Conflict")
+    case ode4xx if Http_Code > 409 and Http_Code < 500:
+        print("Неизвестный код для группы 4xx")
+        
     case 503:
         print("Service Unavailable")
+    case 509:
+        print("Bandwidth Limit Exceeded")
     case 510:
         print("Not Extended")
-        
-if HttpCode > 103 and HttpCode < 200:
-    print("Неизвестный код для группы 1xx")
-elif HttpCode > 207 and HttpCode < 300:
-    print("Неизвестный код для группы 2xx")
-elif HttpCode > 308 and HttpCode < 400:
-    print("Неизвестный код для группы 3xx")
-elif HttpCode > 409 and HttpCode < 500:
-    print("Неизвестный код для группы 4xx")
-elif HttpCode > 511:
-    print("Неизвестный код для группы 5xx")
+    case code5xx if Http_Code > 510:
+        print("Неизвестный код для группы 5xx")
+    
+    case _:
+        print("неизвестный Http_Code")
