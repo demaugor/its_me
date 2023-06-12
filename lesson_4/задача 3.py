1c730166caedcb340935f927d9835a84914b7bee
demaugor <118732056+demaugor@users.noreply.github.com>
2023-06-12 04:21:07 +0400

    Update задача 3.py

--- lesson_4/задача 3.py
+++ lesson_4/задача 3.py
@@ -1,12 +1,12 @@
 factorial = 1
 factorial_2 = 1000
 
-n = 1
+i = 1
 o = 5
 
 while factorial <= factorial_2:
-    if n > 1:
+    if i > 1:
         print(factorial, "==", str(o) + "!")
-    factorial *= n
-    n += 1
+    factorial *= i
+    i += 1
     o += 1
