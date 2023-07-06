06f7b5f4c66e287464556087d0ce4899ce4a88a7
demaugor <118732056+demaugor@users.noreply.github.com>
2023-07-06 22:57:23 +0400

    Create 3 работа версия 1.py

---
+++ lesson_7/3 работа версия 1.py
@@ -0,0 +1,15 @@
+if choice == 'радиус':
+    radius = float(input("Введите радиус круга: "))
+    circumference = 2 * math.pi * radius
+    area = math.pi * radius**2
+    print("Длина окружности:", circumference)
+    print("Площадь круга:", area)
+elif choice == 'диаметр':
+    diameter = float(input("Введите диаметр круга: "))
+    radius = diameter / 2
+    circumference = 2 * math.pi * radius
+    area = math.pi * radius**2
+    print("Длина окружности:", circumference)
+    print("Площадь круга:", area)
+else:
+    print("чёт говной попахивает")
