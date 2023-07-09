5b218f2d6dd7ea937dec58a0d811142f79e3b32f
demaugor <118732056+demaugor@users.noreply.github.com>
2023-07-09 19:36:38 +0400

    терь работает

--- lesson_7/3 работа версия 1.py
+++ lesson_7/3 работа версия 1.py
@@ -1,10 +1,13 @@
-if choice == 'радиус':
+import math
+choice = input("Введите 'радиус' или 'диаметр': ")
+
+if choice == "радиус":
     radius = float(input("Введите радиус круга: "))
     circumference = 2 * math.pi * radius
     area = math.pi * radius**2
     print("Длина окружности:", circumference)
     print("Площадь круга:", area)
-elif choice == 'диаметр':
+elif choice == "диаметр":
     diameter = float(input("Введите диаметр круга: "))
     radius = diameter / 2
     circumference = 2 * math.pi * radius
@@ -12,4 +15,4 @@ elif choice == 'диаметр':
     print("Длина окружности:", circumference)
     print("Площадь круга:", area)
 else:
-    print("чёт говной попахивает")
+    print("Некорректный выбор")
