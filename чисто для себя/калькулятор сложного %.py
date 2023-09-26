f5be7ed0229dc4996f7849e36e023bb613f4b9d7
demaugor <118732056+demaugor@users.noreply.github.com>
2023-09-26 22:57:21 +0400

    ,

---
+++ чисто для себя/калькулятор сложного %.py
@@ -0,0 +1,13 @@
+initial = deposit = int(input("депозин: "))
+percent = float(input("процент: "))/100
+print(f'начальный депозит: {initial}, годовой процент: {percent*100}')
+
+
+for month in range(12):
+    income = deposit * percent / 12
+    deposit += income
+    print(f'месяц: {month+1}, доход: {income}, вклад: {deposit}')
+
+
+year_percent = (deposit - initial) / initial
+print(f'годовой процент с учётом капитализации составил: {year_percent*100}')
